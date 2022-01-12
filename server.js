f3542cd4e9fca6d172f72834b8fbccf940c1dc7c
ArjumanSreashtho <arjumansreashtho@gmail.com>
2022-01-13 01:56:21 +0600

    example code for aamarPay integration  in nodejs

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const axios = require("axios");
+require("colors");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+app.post("/payment", async (req, res, next) => {
+  const { cus_email, cus_name, cus_phone, amount, desc, currency } = req.body;
+  //Fill formData with your own data
+  const formData = {
+    cus_name,
+    cus_email,
+    cus_phone,
+    amount,
+    tran_id: uuid(),
+    signature_key: "dbb74894e82415a2f7ff0ec3a97e4183",
+    store_id: "aamarpaytest",
+    currency,
+    desc,
+    cus_add1: "53, Gausul Azam Road, Sector-14, Dhaka, Bangladesh",
+    cus_add2: "Dhaka",
+    cus_city: "Dhaka",
+    cus_country: "Bangladesh",
+    success_url: "http://localhost:3900/callback",
+    fail_url: "http://localhost:3900/callback",
+    cancel_url: "http://localhost:3900/callback",
+    type: "json", //This is must required for JSON request
+  };
+  const { data } = await axios.post(
+    "https://sandbox.aamarpay.com/jsonpost.php",
+    formData
+  );
+  if (data.result !== "true") {
+    let errorMessage = "";
+    for (let key in data) {
+      errorMessage += data[key] + ". ";
+    }
+    return res.render("error", {
+      title: "Error",
+      errorMessage,
+    });
+  }
+
+  res.status(301).redirect(data.payment_url);
+});
+
+app.post("/callback", async (req, res, next) => {
+  // Callback data
+  // console.log(req.body);
+  const {
+    pay_status,
+    cus_name,
+    cus_phone,
+    cus_email,
+    currency,
+    pay_time,
+    amount,
+  } = req.body;
+  res.render("callback", {
+    title: "Payment Status",
+    pay_status,
+    cus_name,
+    cus_phone,
+    cus_email,
+    currency,
+    pay_time,
+    amount,
+  });
+});
+
+const port = 3900;
+
+app.listen(port, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port: ${port}`.green
+      .bold
+  );
+});
